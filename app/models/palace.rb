0eb1c4d896546c10bea0b7f30e7f19b7d61aa9e2
torihuang <victoriahuang20@gmail.com>
2016-04-05 09:22:20 -0500

    Create seed data

--- app/models/palace.rb
+++ app/models/palace.rb
@@ -1,4 +1,5 @@
 class Palace < ActiveRecord::Base
   belongs_to :room
   belongs_to :creator, class_name: 'User'
+  belongs_to :master, class_name: 'User'
 end
---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+10.times do
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Superhero.power, email: Faker::Internet.email, birthday: Faker::Date.between_except(90.year.ago, 1.year.ago, Date.today))
+end
+
+5.times do
+  Room.create(name: Faker::Hipster.word, image_path: Faker::Avatar.image)
+end
+
+30.times do
+  Object.create(name: Faker::Hipster.word, image_path: Faker::Avatar.image)
+end
+
+50.times do
+  random_creator = User.all.to_a.sample
+  random_master = User.all.to_a.sample
+  random_room = Room.all.to_a.sample
+  Palace.create(name: Faker::Hipster.word, creator: random_creator, room: random_room, master: random_master)
+end
+
+prng = Random.new
+100.times do
+  random_room = Room.all.to_a.sample
+  random_object = Object.all.to_a.sample
+  Placement.create(room: random_room, master: random_master, object: random_object, object_location_x: prng.rand(1000), object_location_y: prng.rand(1000))
+end
