40760d3b55f72b5cbfb0f5cb16e90059a283ffe7
Tori Huang <victoriahuang20@gmail.com>
2016-04-05 10:33:56 -0500

    Create splash page and add validations

--- app/models/palace.rb
+++ app/models/palace.rb
@@ -2,4 +2,6 @@ class Palace < ActiveRecord::Base
   belongs_to :room
   belongs_to :creator, class_name: 'User'
   belongs_to :master, class_name: 'User'
+
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :palaces, foreign_key: :creator_id
   has_many :rooms, through: :palaces, source: :room
   has_many :items, through: :rooms, source: :items
+
+  validates :first_name, :last_name, :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 end
